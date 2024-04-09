f9342716ec81c51e7406aa6f445938261a10fe53
nguyenvanduyb <nguyen.van.duy-b@sun-asterisk.com>
2024-04-09 14:19:08 +0700

    [Task][01] init project

---
+++ iOSCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSCoreData
+//
+//  Created by nguyen.van.duyb on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
